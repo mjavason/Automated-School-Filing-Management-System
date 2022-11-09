409cd9a47896eae8f5f37654ceb2906523c054ef
Michael Orji <orjimichael4886@gmail.com>
2022-11-09 00:03:43 -0800

    Further page clean up and dynamization

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<footer id="footer" class="footer-texts-more-lighten">
+	
+	<div class="container">
+		<div class="footer-copyright footer-copyright-style-2 pt-4 pb-5">
+			<div class="row">
+				<div class="col-12 text-center">
+					<p class="mb-0">Porto Template © 2022. All Rights Reserved</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
